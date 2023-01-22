dacfc8c542d9a20f0158d26ed707fd1556e797ff
NekoCWD <nekodevelopper@gmail.com>
2023-01-22 23:42:57 +0300

    xrdb update 6.4

--- config.def.h
+++ config.def.h
@@ -127,6 +127,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ControlMask,           XK_h,      tagmon,         {.i = -1 } },
 	{ MODKEY|ControlMask,           XK_k, 	   tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_F5,     xrdb,           {.v = NULL } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
