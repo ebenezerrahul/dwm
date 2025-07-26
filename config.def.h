afc58666cc99e607b5b1d1e83390e387d11e1f18
ebenezerrahul <ebenezerrahul@gmail.com>
2025-07-27 01:44:31 +0530

    changed default terminal to kitty

--- config.def.h
+++ config.def.h
@@ -10,8 +10,8 @@ static const unsigned int gappov    = 16;       /* vert outer gap between window
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char dmenufont[]       = "JetBrainsMonoNL NF:style=Regular:pixelsize=32";
-static char font[]            = "JetBrainsMonoNL";
+static const char dmenufont[]       = "JetBrainsMonoNL NF:style=Regular:size=32";
+static char font[]            = "JetBrainsMonoNL NF:style=Regular;size=32";
 static const char *fonts[]          = { font };
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
@@ -70,7 +70,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 static const char *browsercmd[]  = { "firefox", NULL };
 
 /*
