8ea918c9fb53a46e6d81d2fd42481a18df316f25
ebenezerrahul <ebenezerrahul@gmail.com>
2025-07-26 16:11:45 +0530

    change font to Jetbrains

--- config.def.h
+++ config.def.h
@@ -10,8 +10,8 @@ static const unsigned int gappov    = 16;       /* vert outer gap between window
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char dmenufont[]       = "monospace:size=24";
-static char font[]            = "monospace:size=24";
+static const char dmenufont[]       = "JetBrainsMonoNL NF:style=Regular:pixelsize=32";
+static char font[]            = "JetBrainsMonoNL";
 static const char *fonts[]          = { font };
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
